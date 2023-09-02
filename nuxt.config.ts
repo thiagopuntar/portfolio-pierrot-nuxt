9e803df4101844bb6f77066572da5eac728b436f
Leonardo <oliveirabalsa2@gmail.com>
2023-09-02 16:37:28 -0300

    feat(sanity): add sanity to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/sanity'],
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: 'production',
+    useCdn: true,
+    apiVersion: '2021-03-25',
+  },
   css: ['@/assets/css/global.css'],
 });
--- sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export default createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: 'production',
-  useCdn: true,
-  apiVersion: '2021-03-25',
-});
