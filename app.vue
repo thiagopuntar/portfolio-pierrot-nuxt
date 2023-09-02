0282c812ede477ca571c43d4e3012a68e669b740
Leonardo <oliveirabalsa2@gmail.com>
2023-09-02 17:03:12 -0300

    refactor(sanity): add request to server side

--- app.vue
+++ app.vue
@@ -1,28 +1,21 @@
 <script setup lang="ts">
 const { fetchColorsWithRetry } = useUtils();
 
-onMounted(async () => {
-  try {
-    const query = groq`*[_type == "color"]`;
-    const sanity = useSanity();
+const query = groq`*[_type == "color"]`;
+const sanity = useSanity();
+const data: any[] = await fetchColorsWithRetry(sanity, query);
+onMounted(() => {
+  const root = document.documentElement;
 
-    const data: any[] = await fetchColorsWithRetry(sanity, query);
-    console.log('Sanity Client Response:', data);
-
-    const root = document.documentElement;
-    data.forEach((color: any) => {
-      const { name, hexValue } = color;
-      root.style.setProperty(`--${name}`, hexValue);
-    });
-  } catch (error) {
-    console.error('Error fetching data from Sanity:', error);
-  }
+  data.forEach((color: any) => {
+    const { name, hexValue } = color;
+    root.style.setProperty(`--${name}`, hexValue);
+  });
 });
 </script>
 
 <template>
   <div>
-    <div />
     <NuxtPage />
   </div>
 </template>
