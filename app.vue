2b09b468d81de2fe9acbb5af106ab83ecc22b4b0
Leonardo <oliveirabalsa2@gmail.com>
2023-09-02 16:37:59 -0300

    feat(sanity): add function to get colors

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+const { fetchColorsWithRetry } = useUtils();
+
+onMounted(async () => {
+  try {
+    const query = groq`*[_type == "color"]`;
+    const sanity = useSanity();
+
+    const data: any[] = await fetchColorsWithRetry(sanity, query);
+    console.log('Sanity Client Response:', data);
+
+    const root = document.documentElement;
+    data.forEach((color: any) => {
+      const { name, hexValue } = color;
+      root.style.setProperty(`--${name}`, hexValue);
+    });
+  } catch (error) {
+    console.error('Error fetching data from Sanity:', error);
+  }
+});
+</script>
+
 <template>
   <div>
     <div />
